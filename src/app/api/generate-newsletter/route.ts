4f1cb3e8af5c114e4ed8b31e9412bfcd265ec23a
Bikash Mishra <mishrabikash202@gmail.com>
2025-01-31 21:21:20 +0530

    config remove

--- src/app/api/generate-newsletter/route.ts
+++ src/app/api/generate-newsletter/route.ts
@@ -11,9 +11,6 @@ interface PlanInterface {
   newsletterSections: [{ title: string; description: string }];
 }
 
-export const config = {
-  maxDuration: 500,
-};
 
 export async function POST(req: Request) {
   //get topic,tone,audience
